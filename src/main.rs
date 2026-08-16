4772ae8cd2c8e17e6436a09048b869f2618d5e42
Gustavo A. Verde <gustavo.verde18@gmail.com>
2026-08-15 23:02:05 -0500

    [WORKING CORRECTLY] Refactor adding a specifict function and data structure to the configuration variables, using references.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("Searching for {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
+
+    println!("With text:\n{contents}")
+}
+
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
+}
+
+fn parse_config(args: &[String]) -> Config<'_> {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {query}");
-    println!("Searching for {file_path}");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
-
-    println!("With text:\n{contents}")
+    Config { query, file_path }
 }
