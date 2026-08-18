27ee02683695eb84e2068b384f1891df7c52f355
Gustavo A. Verde <gustavo.verde18@gmail.com>
2026-08-17 21:43:37 -0500

    Handling errors returned from run in main.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("Searching for {}", config.file_path);
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
-
-    println!("With text:\n{contents}")
+    if let Err(e) = run(&config) {
+        println!("Application error: {e}");
+        process::exit(1)
+    }
 }
 
 struct Config<'a> {
@@ -33,4 +32,14 @@ impl Config<'_> {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(configuration: &Config) -> Result<(), Box<dyn Error>> {
+    println!("Searching for {}", configuration.query);
+    println!("Searching for {}", configuration.file_path);
+
+    let contents = fs::read_to_string(configuration.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
